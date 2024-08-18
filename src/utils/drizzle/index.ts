823ba398c0d325f8c0585b38a71b1121a80f7b76
🌻 jaak 🌻 <jacques@mohara.co>
2024-08-18 09:47:37 +0000

    [setup]: working on setting up the drizzle connection

---
+++ src/utils/drizzle/index.ts
@@ -0,0 +1,35 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Client } from 'pg';
+
+import { labeledLogger } from '../logger';
+
+const { info, error } = labeledLogger('utils:drizzle');
+
+/**
+ * Configures and returns a PostgreSQL client.
+ * @async
+ * @return {Promise<Client>} A promise that resolves to a configured PostgreSQL client.
+ */
+const configureClient = async () => {
+  try {
+    info('Configuring Drizzle client.');
+    const clientConfig = new Client({
+      host: process.env.DATABASE_HOST as string,
+      port: parseInt(process.env.DATABASE_PORT as string, 10),
+      user: process.env.POSTGRES_USER as string,
+      password: process.env.POSTGRES_PASSWORD as string,
+      database: process.env.POSTGRES_DB as string,
+    });
+
+    info('Drizzle client configured. Establishing connection.');
+    await clientConfig.connect();
+
+    info('Drizzle client connection established.');
+    return drizzle(clientConfig);
+  } catch (e) {
+    error('Drizzle client configuration failed.', e);
+    throw e;
+  }
+};
+
+export default configureClient;
