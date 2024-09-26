4a63ef652713ef5a247bdccf0c4cd6200e6fb400
🌻 jaak 🌻 <jacques@mohara.co>
2024-09-26 16:43:25 +0000

    feat (database): adjusted the cost_base to be nullable

--- src/modules/campaign/controller.ts
+++ src/modules/campaign/controller.ts
@@ -1,21 +1,50 @@
 import { NextFunction, Request, Response } from 'express';
 
-import logger from '@modules/logger';
+import { labeledLogger } from '@modules/logger';
+import { createCampaign as createNewCampaign } from '.';
 
+const logger = labeledLogger('module:campaign/controller');
+
+/**
+ * Retrieves campaign details based on the campaign ID provided in the request parameters.
+ *
+ * @param {Request} req The request object containing the campaign ID in the parameters.
+ * @param {Response} res The response object used to send the response.
+ * @param {NextFunction} _next The next middleware function in the stack (unused).
+ * @return {Promise<void>} A promise that resolves to void.
+ */
 export const campaignDetails = async (
   req: Request,
   res: Response,
   _next: NextFunction,
-) => {
+): Promise<void> => {
   logger.info(`Retrieving campaign details for id: ${req.params.id}`);
-  return res.json({ status: 'ok' });
+  res.json({ status: 'ok' });
 };
 
+/**
+ * Creates a new campaign.
+ *
+ * @param {Request} req The request object (unused).
+ * @param {Response} res The response object used to send the response.
+ * @param {NextFunction} _next The next middleware function in the stack (unused).
+ * @return {Promise<void>} A promise that resolves to void.
+ */
 export const createCampaign = async (
-  _req: Request,
+  req: Request,
   res: Response,
-  _next: NextFunction,
-) => {
-  logger.info('Creating campaign.');
-  return res.json({ status: 'ok' });
+  next: NextFunction,
+): Promise<void> => {
+  const { ...campaignData } = req.body;
+
+  try {
+    const result = await createNewCampaign(campaignData);
+    res.status(200).json({
+      data: {
+        campaign: result,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
---
+++ src/modules/campaign/index.ts
@@ -0,0 +1,19 @@
+import { Campaign } from './types';
+import DbConnection from '@database/client';
+import { labeledLogger } from '../logger';
+import { campaigns } from '@src/database/schema/campaigns';
+
+const logger = labeledLogger('module:campaign');
+const database = new DbConnection().configure();
+
+/**
+ * Creates a new campaign in the database.
+ *
+ * @param {Campaign} campaignData The data of the campaign to create.
+ * @return {Promise<any>} A promise that resolves when the campaign is created.
+ */
+export async function createCampaign(campaignData: Campaign): Promise<any> {
+  logger.info('Creating campaign.');
+  const db = (await database).getDb();
+  return db.insert(campaigns).values(campaignData);
+}
