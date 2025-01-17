80dd398b9e5869f936cf89047a5ae908d96b2248
🌻 jaak 🌻 <code@tri2b.me>
2025-01-17 10:54:20 +0000

    feat (database): added the buyers table

--- src/modules/user/repository.ts
+++ src/modules/user/repository.ts
@@ -1,7 +1,7 @@
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 
 import { users } from '@database/schema/users';
-import { sellers } from '@database/schema/sellers';
+import { buyers } from '@database/schema/buyers';
 import { User } from './types';
 import { DatabaseNotDefinedError } from './errors';
 
@@ -56,6 +56,42 @@ export async function createSeller(
   } catch (error) {
     console.error('Error creating seller:', error);
     throw new Error('Failed to create seller');
+  }
+}
+
+/**
+ * Creates a new buyer and corresponding user in the database.
+ *
+ * @param userData - An object containing user data.
+ * @param db - The database connection instance.
+ * @returns The created buyer record or an error message.
+ */
+export async function createBuyer(
+  userData: User,
+  db: NodePgDatabase<Record<string, never>> | undefined,
+): Promise<any> {
+  if (!db) {
+    throw new DatabaseNotDefinedError();
+  }
+
+  try {
+    // Insert the user and retrieve the generated ID
+    const [newUser] = await db
+      .insert(users)
+      .values(userData)
+      .returning({ id: users.id });
+
+    // Insert the buyer using the retrieved user ID
+    const [newBuyer] = await db
+      .insert(buyers)
+      .values({ userId: newUser.id })
+      .returning({ id: buyers.id });
+
+    // Return the created buyer record
+    return newBuyer;
+  } catch (error) {
+    console.error('Error creating buyer:', error);
+    throw new Error('Failed to create buyer');
   }
 }
 
