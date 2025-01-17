1fc358803a800c0ec8bce30a71566dc13f08b254
🌻 jaak 🌻 <code@tri2b.me>
2025-01-17 14:29:31 +0000

    feat (tradesafe): initiated npm in the directory

--- src/modules/transaction/controller.ts
+++ src/modules/transaction/controller.ts
@@ -3,30 +3,37 @@ import { labeledLogger } from '../logger';
 import { createNewTransaction } from '.';
 
 const logger = labeledLogger('module:transaction/controller');
-
+/**
+ * Creates a new transaction in the database.
+ * @param request - The incoming HTTP request object containing transaction data.
+ * @param response - The HTTP response object used to send back the response.
+ * @param next - The next middleware function in the Express stack.
+ * @returns A Promise that resolves to the created transaction or calls the next middleware with an error.
+ */
 export async function createTransaction(
   request: Request,
   response: Response,
   next: NextFunction,
 ): Promise<Response | void> {
-  let {
-    campaignId = null,
-    buyerId = null,
-    amount = null,
-    transactionId = null,
-  } = request.body;
-  logger.info(
-    `Creating new transaction from buyerId: ${buyerId} with amount of ${amount} for campaignId: ${campaignId}.`,
-  );
+  try {
+    const { campaignId = null, buyerId = null, amount = null } = request.body;
 
-  if (transactionId === null) transactionId = 'test-transaction';
+    logger.info(
+      `Creating new transaction from buyerId: ${buyerId} with amount of ${amount} for campaignId: ${campaignId}.`,
+    );
 
-  const transaction = await createNewTransaction({
-    amount,
-    campaignId,
-    buyerId,
-    transactionId,
-  });
+    
 
-  response.status(200).json({ data: transaction });
+    const transaction = await createNewTransaction({
+      amount,
+      campaignId,
+      buyerId,
+      transactionId,
+    });
+
+    return response.status(200).json({ data: transaction });
+  } catch (error) {
+    logger.error('Error creating transaction:', error);
+    next(error);
+  }
 }
